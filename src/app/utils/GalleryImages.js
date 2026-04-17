666768cf1a863a25a24a0e4f963a1561036f4132
Alex Echeverria <aleex.naahuel@outlook.com>
2026-04-17 01:36:01 -0300

    agrego imagenes para la  galeria

--- src/app/utils/GalleryImages.js
+++ src/app/utils/GalleryImages.js
@@ -9,11 +9,21 @@ import trishot from '../../../assets/products/trishot.png';
 import mogul from '../../../assets/products/mogul.png';
 import saladix from '../../../assets/products/saladix.png';
 
-export const galleryImages = [
+import casamiento from '../../../assets/experience/casamiento.png';
+import empresarial from '../../../assets/experience/empresarial.png';
+import infantil from '../../../assets/experience/infantil.png';
+import quince from '../../../assets/experience/quince.png';
+
+
+// link wpp https://wa.me/c/5492235401000
+
+export const galleryProductsImages = [
+  /*
   {
     url: milka,
     title: 'Candy Bar Colorido',
   },
+  */
   {
     url: bubbaloo,
     title: 'Bubbaloo',
@@ -47,25 +57,29 @@ export const galleryImages = [
   },
   {    url: saladix,
     title: 'Saladix',
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1574004897612-c6bbad355614?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx3ZWRkaW5nJTIwY2FuZHklMjB0YWJsZXxlbnwxfHx8fDE3NzQ4MjEwMjF8MA&ixlib=rb-4.1.0&q=80&w=1080',
-    title: 'Mesa de Casamiento',
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1772411534568-1fe0c61b21a3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjb2xvcmZ1bCUyMG1hY2Fyb25zJTIwZGlzcGxheXxlbnwxfHx8fDE3NzQ4MjEwMjJ8MA&ixlib=rb-4.1.0&q=80&w=1080',
-    title: 'Macarons Artesanales',
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1764380746818-18c01e96df12?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjaG9jb2xhdGUlMjBkZXNzZXJ0cyUyMGVsZWdhbnR8ZW58MXx8fHwxNzc0ODIxMDIyfDA&ixlib=rb-4.1.0&q=80&w=1080',
-    title: 'Postres de Chocolate',
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1770021999036-53291a3a6596?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjYW5keSUyMHNob3AlMjBhc3NvcnRtZW50fGVufDF8fHx8MTc3NDgyMTAyM3ww&ixlib=rb-4.1.0&q=80&w=1080',
-    title: 'Surtido de Golosinas',
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1705276920624-cd72e0f89dd9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxxdWluY2VhbmVyYSUyMHBhcnR5JTIwZGVzc2VydHN8ZW58MXx8fHwxNzc0ODIxMDIyfDA&ixlib=rb-4.1.0&q=80&w=1080',
-    title: 'Dulces Personalizados',
-  },
+  }
 ];
+
+export const galleryExperienceImages = [
+  {
+    url: casamiento,
+    title: 'Casamiento',
+  },
+  {
+    url: empresarial,
+    title: 'Empresarial',
+  },
+  {
+    url: infantil,
+    title: 'Infantil',
+  },
+  {
+    url: quince,
+    title: 'Quince',
+  }
+
+
+];
+
+
+
